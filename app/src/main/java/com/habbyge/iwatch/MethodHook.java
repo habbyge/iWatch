c5d592521dd972e1f72928a8bd3a974f4e05145f
habbyge <habbyge@tencent.com>
2021-01-15 14:08:37 +0800

    优化内存泄漏、恢复机制

--- app/src/main/java/com/habbyge/iwatch/MethodHook.java
+++ app/src/main/java/com/habbyge/iwatch/MethodHook.java
@@ -8,7 +8,6 @@ import androidx.annotation.Keep;
 import com.habbyge.iwatch.util.ReflectUtil;
 
 import java.lang.reflect.Field;
-import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 
 /**
