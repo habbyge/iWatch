66040ed580b0affacddb4daa89dffc44a4784028
habbyge <habbyge@tencent.com>
2021-02-28 13:15:06 +0800

    ignore

--- app/src/main/java/com/habbyge/iwatch/util/Type.java
+++ app/src/main/java/com/habbyge/iwatch/util/Type.java
@@ -1,25 +1,7 @@
 package com.habbyge.iwatch.util;
 
 public final class Type {
-    public static final int ARRAY = 9;
-    public static final int OBJECT = 10;
-
-    private final int sort;
-    private final char[] buf;
-    private final int off;
-    private final int len;
-
-    private Type(final int sort, final char[] buf, final int off, final int len) {
-        this.sort = sort;
-        this.buf = buf;
-        this.off = off;
-        this.len = len;
-    }
-
-    public String getDescriptor() {
-        StringBuilder buf = new StringBuilder();
-        getDescriptor(buf);
-        return buf.toString();
+    private Type() {
     }
 
     public static String getMethodDescriptor(final Class<?> returnType, final Class<?>... paramTypes) {
@@ -34,20 +16,6 @@ public final class Type {
 
         getDescriptor(buf, returnType);
         return buf.toString();
-    }
-
-    private void getDescriptor(final StringBuilder buf) {
-        if (this.buf == null) {
-            // descriptor is in byte 3 of 'off' for primitive types (buf ==
-            // null)
-            buf.append((char) ((off & 0xFF000000) >>> 24));
-        } else if (sort == OBJECT) {
-            buf.append('L');
-            buf.append(this.buf, off, len);
-            buf.append(';');
-        } else { // sort == ARRAY || sort == METHOD
-            buf.append(this.buf, off, len);
-        }
     }
 
     private static void getDescriptor(final StringBuilder buf, final Class<?> c) {
@@ -96,46 +64,5 @@ public final class Type {
                 return;
             }
         }
-    }
-
-    @Override
-    public boolean equals(final Object o) {
-        if (this == o) {
-            return true;
-        }
-        if (!(o instanceof Type)) {
-            return false;
-        }
-        Type t = (Type) o;
-        if (sort != t.sort) {
-            return false;
-        }
-        if (sort >= ARRAY) {
-            if (len != t.len) {
-                return false;
-            }
-            for (int i = off, j = t.off, end = i + len; i < end; i++, j++) {
-                if (buf[i] != t.buf[j]) {
-                    return false;
-                }
-            }
-        }
-        return true;
-    }
-
-    @Override
-    public int hashCode() {
-        int hc = 13 * sort;
-        if (sort >= ARRAY) {
-            for (int i = off, end = i + len; i < end; i++) {
-                hc = 17 * (hc + buf[i]);
-            }
-        }
-        return hc;
-    }
-
-    @Override
-    public String toString() {
-        return getDescriptor();
     }
 }
