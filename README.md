8bdf79476708f8161c39950444b9b8a0d6564013
habbyge <habbyge@tencent.com>
2021-03-19 23:47:11 +0800

    测试样例

--- README.md
+++ README.md
@@ -22,11 +22,20 @@
 adb push 补丁.apatch 到手机这个目录: /storage/emulated/0/Android/data/com.habbyge.iwatch/files/Music/
 
 ## 问题和限制(todo......有待突破)
-1. 对 ***内部类*** 有限制，在内部类中需要新增调用方法时，则需要直接新建一个class然后调用该class，直接在内部类中调用其外部类则容易造成方法地址和index错乱，找不到方法，发生NoSuchMethodError crash。但是非内部类场景没有这个限制。不过还是建议新建class的方式来搞。
+1. 对 ***内部类*** 有限制，在内部类中需要新增调用方法时，则需要直接新建一个class然后调用该class，直接在内部类中调用其外部类则容易
+   造成方法地址和index错乱，找不到方法，发生NoSuchMethodError crash。但是非内部类场景没有这个限制。不过还是建议新建class的方式来搞。
 2. 内部类中修改需要访问外部类时，其访问的method、field、都需要设置为public，是为了阻止编译器自动合成synthric方法，造成方法地址或index错乱。
 3. inline的目标函数，一般不能fix。
 4. ......
 
-### 内部类的坑
+## 坑(todo)
+### 1. 内部类的坑
 内部类引用外部类的哪个方法，就会找不到该方法。得出结论，一定是内部类改变了该方法。todo
-java.lang.NoSuchMethodError: No virtual method b(Ljava/lang/String;)V in class Lcom/habbyge/sample/MainActivity_CF; or its super classes (declaration of 'com.habbyge.sample.MainActivity_CF' appears in /storage/emulated/0/Android/data/com.habbyge.iwatch/files/Music/app-release-2-cfff1f13df8681ea51edb3ea824af973.apatch)
+java.lang.NoSuchMethodError: No virtual method b(Ljava/lang/String;)V in class Lcom/habbyge/sample/MainActivity_CF;
+or its super classes (declaration of 'com.habbyge.sample.MainActivity_CF' appears in
+/storage/emulated/0/Android/data/com.habbyge.iwatch/files/Music/app-release-2-cfff1f13df8681ea51edb3ea824af973.apatch)
+
+### 2. 调用方法的坑
+在开启proguard混淆、优化时，非内部类中方法调用一个修改后方法，需要以当前方法为起点，向上回溯，直到遇到keep住的方法停止，然后在这个被keep住
+的方法插入一个代码(通常打一个日志)，来保证该keep函数也被修改，即被标记为FixMethodAnno，出现在patch文件中；否则会导致：
+NoSuchMethodError exception，具体原因未知，我猜是地址偏移了，后面研究(todo); ------ 得找个方法来阻止地址变更。
--- app/src/main/java/com/habbyge/sample/MainActivity.java
+++ app/src/main/java/com/habbyge/sample/MainActivity.java
@@ -1,85 +1,84 @@
-package com.habbyge.sample;
-
-import android.app.Activity;
-import android.os.Bundle;
-import android.util.Log;
-import android.view.View;
-
-import com.habbyge.iwatch.R;
-
-/**
- * Created by habbyge on 2020/11/24.
- */
-public class MainActivity extends Activity {
-    private static final String TAG = "iWatch.MainActivity";
-
-    // 测试用例: private字段是否可访问
-    public static int ix = 10;
-    public int ix_HOOK = 10000;
-//    public String strX_Added = "Mali"; // 测试用例: 验证新增一个字段
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-    }
-
-    @Override
-    protected void onResume() {
-        super.onResume();
-
-        Log.i(TAG, "onCreate, ix_HOOK=" + ix_HOOK + ", ix=" + ix + ", " + ix);
-
-        findViewById(R.id.method).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Log.i(TAG, "Fix-2, btnHookMethod, onClick !"); // 测试用例：验证内部类改变
-
-                // 测试用例：内部类访问外部现有private字段(static or not)会失败，因为编译期会为private字段合成
-                // 一个访问方法（类型flag：Synthetic），该访问方法的形参是旧的修复前的类型(对象)，但是函数中是修复
-                // 类名，因此发生failed to verify的crash，因此必须修改成public才能阻止其生成合成方法。
-                ix = 1;
-                ix_HOOK = 10;
-                Log.d(TAG, "HABBYGE-MALI, ix=" + ix + ", ix_HOOK=" + ix_HOOK);
-
-                // 测试用例：测试(匿名)内部类访问新增的class(包括新增字段、方法)
-//                Test test = new Test();
-//                test.print("i love my family ! new !!!!!!");
-
-                // 测试用例：测试(匿名)内部类访问现有方法(public/private)
-//                printf("onClick");
-            }
-        });
-
-        printf2("onCreate-End", 1000);
-    }
-
-    // 测试用例: 验证新增方法、访问类中字段......
-    public void printf2(String text, int x1) { // 通过修改 printf(text + x1);
-        for (int i = 0; i < 100; ++i) {
-            ++ix_HOOK;
-            ++ix;
-            ++x1;
-        }
-
-        printf(text + x1);
-    }
-
-    // 测试用例: 验证新增方法、访问类中字段......
-    public void printf(String text) { // 通过修改
-        Log.w(TAG, "printf-bengin: " + text + ", ix=" + ix + "，beg !!!!!!");
-
-        for (int i = 0; i < 100; ++i) {
-            ++ix_HOOK;
-            ++ix;
-        }
-
-        int x = ix * ix_HOOK;
-
-        Log.d(TAG, "printf-end: " + text + ", ix_HOOK2="
-                + ix_HOOK + ", " /*+ test("ix_HOOK_ix")*/
-                + ", x = " + x + ", xx=" + text);
-
-        Log.w(TAG, "printf-bengin: " + text + ", ix=" + ix + ", x1=" + "，end !!!!!!");
-    }
-}
+//package com.habbyge.sample;
+//
+//import android.app.Activity;
+//import android.os.Bundle;
+//import android.util.Log;
+//import android.view.View;
+//
+//import com.habbyge.iwatch.R;
+//
+///**
+// * Created by habbyge on 2020/11/24.
+// */
+//public class MainActivity extends Activity {
+//    private static final String TAG = "iWatch.MainActivity";
+//
+//    // 测试用例: private字段是否可访问
+//    public static int ix = 10;
+//    public int ix_HOOK = 10000;
+////    public String strX_Added = "Mali"; // 测试用例: 验证新增一个字段
+//
+//    @Override
+//    protected void onCreate(Bundle savedInstanceState) {
+//        super.onCreate(savedInstanceState);
+//        setContentView(R.layout.activity_main);
+//    }
+//
+//    @Override
+//    protected void onResume() {
+//        super.onResume();
+//
+//        Log.i(TAG, "onResume, ix_HOOK=" + ix_HOOK + ", ix=" + ix + ", " + ix);
+//
+//        findViewById(R.id.method).setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                Log.i(TAG, "Fix-2, btnHookMethod, onClick !"); // 测试用例：验证内部类改变
+//
+//                // 测试用例：内部类访问外部现有private字段(static or not)会失败，因为编译期会为private字段合成
+//                // 一个访问方法（类型flag：Synthetic），该访问方法的形参是旧的修复前的类型(对象)，但是函数中是修复
+//                // 类名，因此发生failed to verify的crash，因此必须修改成public才能阻止其生成合成方法。
+//                ix = 1;
+//                ix_HOOK = 10;
+//                Log.d(TAG, "HABBYGE-MALI, ix=" + ix + ", ix_HOOK=" + ix_HOOK);
+//
+//                // 测试用例：测试(匿名)内部类访问新增的class(包括新增字段、方法)
+////                Test test = new Test();
+////                test.print("i love my family ! new !!!!!!");
+//
+//                // 测试用例：测试(匿名)内部类访问现有方法(public/private)
+////                printf("onClick");
+//            }
+//        });
+//
+//        printf2("onCreate-End", 100);
+//    }
+//
+//    // 测试用例: 验证新增方法、访问类中字段......
+//    public void printf2(String text, int x1) { // 通过修改 printf(text + x1);
+//        for (int i = 0; i < 100; ++i) {
+//            ++ix_HOOK;
+//            ++ix;
+//            ++x1;
+//        }
+//        printf(text + x1);
+//    }
+//
+//    // 测试用例: 验证新增方法、访问类中字段......
+//    public void printf(String text) { // 通过修改
+////        Log.w(TAG, "printf-bengin-1: " + text + ", ix=" + ix + "，beg !!!!!!");
+//
+//        for (int i = 0; i < 100; ++i) {
+//            ++ix_HOOK;
+//            ++ix;
+//        }
+//
+//        int x = ix * ix_HOOK;
+//
+//        Log.d(TAG, "printf-end-2: " + text + ", ix_HOOK2="
+//                + ix_HOOK + ", " /*+ test("ix_HOOK_ix")*/
+//                + ", x = " + x + ", xx=" + text);
+//
+//        Log.w(TAG, "printf-bengin-2: " + text + ", ix=" + ix + ", x1=" + "，end !!!!!!");
+//    }
+//}
