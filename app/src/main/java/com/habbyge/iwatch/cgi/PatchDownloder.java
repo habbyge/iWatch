3995423ea9b33f8ad6ab0c9d3903868e76dad638
habbyge <habbyge@tencent.com>
2021-03-28 01:49:41 +0800

    补丁文件下载器

--- app/src/main/java/com/habbyge/iwatch/cgi/PatchDownloder.java
+++ app/src/main/java/com/habbyge/iwatch/cgi/PatchDownloder.java
@@ -1,15 +1,29 @@
 package com.habbyge.iwatch.cgi;
 
 import android.app.DownloadManager;
+import android.content.BroadcastReceiver;
 import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
 import android.net.Uri;
+import android.util.Log;
 
 /**
  * Created by habbyge on 2021/1/5.
  */
 public final class PatchDownloder {
+    private static final String TAG = "iWatch.PatchDownloder";
 
-    void request(Context context, String url, IPatchDownloadCallback callback) {
+    private long downloadId = 0L;
+    private IPatchDownloadCallback callback;
+
+    public PatchDownloder(IPatchDownloadCallback callback) {
+        this.callback = callback;
+    }
+
+    public void request(Context context, String url) {
+        context.registerReceiver(receiver, new IntentFilter(DownloadManager.ACTION_DOWNLOAD_COMPLETE));
+
         DownloadManager dm = (DownloadManager) context.getSystemService(Context.DOWNLOAD_SERVICE);
         DownloadManager.Request request = new DownloadManager.Request(Uri.parse(url));
 
@@ -23,6 +37,20 @@ public final class PatchDownloder {
         // 表示下载允许的网络类型
         request.setAllowedNetworkTypes(DownloadManager.Request.NETWORK_WIFI);
 
-        long downloadId = dm.enqueue(request);
+        downloadId = dm.enqueue(request);
     }
+
+    private final BroadcastReceiver receiver = new BroadcastReceiver() {
+
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            String action = intent.getAction();
+            if (DownloadManager.ACTION_DOWNLOAD_COMPLETE.equals(action)) {
+                Log.i(TAG, "onReceive, action = " + action);
+                if (callback != null) {
+                    callback.onCallback(); // TODO: 2021/3/28
+                }
+            }
+        }
+    };
 }
