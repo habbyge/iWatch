ffbd278c3c047cfcee1f8eaca39769032013687f
habbyge <habbyge@tencent.com>
2021-03-31 19:40:10 +0800

    删除md5

--- app/src/main/java/com/habbyge/iwatch/patch/Patch.java
+++ app/src/main/java/com/habbyge/iwatch/patch/Patch.java
@@ -18,7 +18,7 @@ import java.util.jar.Manifest;
  * Created by habbyge on 2021/1/5.
  */
 public class Patch implements Comparable<Patch> {
-    private static final String TAG = "iWatch.Patch";
+    private static final String TAG = "dungfork.Patch";
 
     private static final String ENTRY_NAME = "META-INF/PATCH.MF";
     private static final String PATCH_CLASSES = "Patch-Classes"; // 包括新增、修改的类
@@ -34,7 +34,6 @@ public class Patch implements Comparable<Patch> {
     private final File mFile; // patch 文件名是固定的，也就是路径是固定的
     private String mName;
     private Date mTime;
-    private String mPatchVersion;
     private String mBaseAppVersion;
 
     // 该 Patch 中包含的所有Class
@@ -56,7 +55,7 @@ public class Patch implements Comparable<Patch> {
      * Created-Time: 26 Feb 2021 07:10:21 GMT
      * From-File: app-release-2.apk
      * To-File: app-release-1.apk
-     * Patch-Classes: com.habbyge.sample.MainActivity$4_CF,com.habbyge.iwatch.MainActivity$5_CF
+     * Patch-Classes: com.tencent.mm.dungfork.sample.MainActivity$4_CF,com.tencent.mm.dungfork.MainActivity$5_CF
      * Created-By: 1.0 (ApkPatch)
      *
      * 其中: "From-File" 表示: 修复包名-补丁版本号.apk "To-File" 表示: 基础包名-版本号.apk
@@ -74,11 +73,11 @@ public class Patch implements Comparable<Patch> {
             // 补丁创建时间
             mTime = new Date(mainAttributes.getValue(CREATED_TIME)); // 26 Feb 2021 16:22:33 GMT
 
-            mPatchVersion = getVersion(mainAttributes.getValue(PATCH_VERSION)); // 补丁版本
+            String patchVersion = getVersion(mainAttributes.getValue(PATCH_VERSION)); // 补丁版本
             mBaseAppVersion = getVersion(mainAttributes.getValue(BASE_APP_VERSION)); // 宿主app版本
 
             Log.i(TAG, "init, mName=" + mName + ", mTime=" + mTime
-                    + ", mPatchVersion=" + mPatchVersion
+                    + ", patchVersion=" + patchVersion
                     + ", mBaseAppVersion=" + mBaseAppVersion);
 
             Attributes.Name attrName;
@@ -103,10 +102,6 @@ public class Patch implements Comparable<Patch> {
                 inputStream.close();
             }
         }
-    }
-
-    public String geVersion() {
-        return mPatchVersion;
     }
 
     public String getName() {
