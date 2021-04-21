c1825417237297c95881bd9bfe5a548da75403f0
habbyge <habbyge@tencent.com>
2021-04-22 01:25:45 +0800

    disable class verfify

--- app/src/main/cpp/runtime.cpp
+++ app/src/main/cpp/runtime.cpp
@@ -8,21 +8,41 @@ namespace iwatch {
 
 void* Runtime::self_ = nullptr;
 
-void Runtime::init(JNIEnv*, const std::shared_ptr<Elf>& elf_op) {
+void Runtime::init(JNIEnv*, const std::shared_ptr<Elf>& elf_op, int sdkVersionCode) {
   void** pInstance_ = reinterpret_cast<void**>(elf_op->dlsym_elf(RUNTIME_INSTANCE_SYM));
   instance_ = *pInstance_;
+  logd("Runtime::init, instance_ = %p", instance_);
+  if (instance_ == nullptr) {
+    return;
+  }
 
-  auto currentFromGdb = reinterpret_cast<CurrentFromGdb>(elf_op->dlsym_elf(CurrentFromGdb_Syn));
+  auto currentFromGdb = reinterpret_cast<CurrentFromGdb>(elf_op->dlsym_elf(CurrentFromGdb_SYM));
   self_ = currentFromGdb();
-  logd("Runtime::init, self_ = %p", self_);
+  logd("Runtime::init, self_ = %p, sdkVersionCode=%d", self_, sdkVersionCode);
 
-  if (sdkVersion > SDK_INT_ANDROID_6_0) {
+  auto isVerificationEnabled = reinterpret_cast<IsVerificationEnabled>(elf_op->dlsym_elf(IsVerificationEnabled_SYM));
+  bool verify_ = isVerificationEnabled(instance_);
+  logd("Runtime::init, isVerificationEnabled = %d", verify_);
+  if (verify_) { // 如果是class校验是打开的，则关闭
+    if (sdkVersionCode >= SDK_INT_ANDROID_9) {
+      auto disableVerifier = reinterpret_cast<DisableVerifier>(elf_op->dlsym_elf(DisableVerifier_SYM));
+      disableVerifier(instance_);
+      // 再次验证是否关闭
+      verify_ = isVerificationEnabled(instance_);
+      logi("Runtime::init, >= android-9, check again, isVerificationEnabled = %d", verify_);
+    } else {
+      // TODO: < 9.0，应该是关闭的
+      logi("Runtime::init, < android-9, check again, isVerificationEnabled = %d", verify_);
+    }
+  }
+
+  /*if (sdkVersionCode > SDK_INT_ANDROID_6_0) {
     useJitCompilation = reinterpret_cast<UseJitCompilation>(elf_op->dlsym_elf(UseJitCompilation_Syn));
-  } else if (sdkVersion == SDK_INT_ANDROID_6_0) {
+  } else if (sdkVersionCode == SDK_INT_ANDROID_6_0) {
     useJitCompilation = reinterpret_cast<UseJitCompilation>(elf_op->dlsym_elf(UseJit_Syn));
   } else {
     useJitCompilation = nullptr; // android-5.x 未采用jit是false
-  }
+  }*/
 }
 
 /**
@@ -33,12 +53,12 @@ void* Runtime::getRuntime() const noexcept {
 }
 
 void* Runtime::currentThread() noexcept {
-  logw("Runtime::init, currentThread=%p", self_);
+  logi("Runtime::init, currentThread=%p", self_);
   return self_;
 }
 
-bool Runtime::useJit() const noexcept {
+/*bool Runtime::useJit() const noexcept {
   return useJitCompilation != nullptr && useJitCompilation();
-}
+}*/
 
-}
+} // namespace iWatch
--- app/src/main/cpp/runtime.h
+++ app/src/main/cpp/runtime.h
@@ -35,8 +35,8 @@ static const char* RUNTIME_INSTANCE_SYM = "_ZN3art7Runtime9instance_E"; // stati
 
 // art/runtime/thread.h 中:
 using CurrentFromGdb = void* (*)(); // Thread* CurrentFromGdb()
-static const char* CurrentFromGdb_Syn = "_ZN3art6Thread14CurrentFromGdbEv";
- // CurrentFromGdb -> _ZN3art6Thread14CurrentFromGdbEv 是否只能在gdb debug 时才能获取？不是，release一样可以获取
+static const char* CurrentFromGdb_SYM = "_ZN3art6Thread14CurrentFromGdbEv";
+// CurrentFromGdb -> _ZN3art6Thread14CurrentFromGdbEv 是否只能在gdb debug 时才能获取？不是，release一样可以获取
 // GetCurrentMethod() -> _ZNK3art6Thread16GetCurrentMethodEPjbb
 // DumpJavaStack -> _ZNK3art6Thread13DumpJavaStackERNSt3__113basic_ostreamIcNS1_11char_traitsIcEEEEbb
 // DumpStack -> _ZNK3art6Thread9DumpStackERNSt3__113basic_ostreamIcNS1_11char_traitsIcEEEEbP12BacktraceMapb
@@ -54,11 +54,70 @@ static const char* CurrentFromGdb_Syn = "_ZN3art6Thread14CurrentFromGdbEv";
 // android 5.0/5.1/6.0(interpreter + aot)
 // android 7.0/7.1及其以后的版本，ART移入了全新的Hybrid模式(interpreter + jit + aot)
 // bool UseJitCompilation() const;
-static const char* UseJitCompilation_Syn = "_ZNK3art7Runtime17UseJitCompilationEv"; // >= android-7.0
-using UseJitCompilation = bool (*)();
+//static const char* UseJitCompilation_Syn = "_ZNK3art7Runtime17UseJitCompilationEv"; // >= android-7.0
+//using UseJitCompilation = bool (*)();
 // bool UseJit() const;
-static const char* UseJit_Syn = "_ZNK3art7Runtime6UseJitEv"; // android-6.x
+//static const char* UseJit_Syn = "_ZNK3art7Runtime6UseJitEv"; // android-6.x
 // android-5.0.5.1 没有这个选项，阅读了源码，5.x是直接采用AOT进程安装编译的
+
+// FIXME: 如果要用jit load或compile功能
+// ------------------ android-11 ------------------
+// JIT compiler
+// static void* jit_library_handle_;
+// static JitCompilerInterface* jit_compiler_;
+// static JitCompilerInterface* (*jit_load_)(void);
+// template <typename T> static bool LoadSymbol(T*, const char* symbol, std::string* error_msg);
+// 在libart.so中查找：
+// flame:/apex/com.android.art/lib64 $ readelf -s libart.so | grep 'jit_compile'
+//  2394: 00000000006ab610     8 OBJECT  GLOBAL PROTECTED 23 _ZN3art3jit3Jit13jit_compiler_E
+// 23991: 00000000006ab610     8 OBJECT  GLOBAL PROTECTED 23 _ZN3art3jit3Jit13jit_compiler_E
+// readelf -s libart.so | grep 'jit_load'
+//  2163: 00000000006ab618     8 OBJECT  GLOBAL PROTECTED 23 _ZN3art3jit3Jit9jit_load_E
+// 24101: 00000000006ab618     8 OBJECT  GLOBAL PROTECTED 23 _ZN3art3jit3Jit9jit_load_E
+
+// ------------------ android-10 ------------------
+// 位于：art/runtime/jit/jit.h
+// 在 art/runtime/jit/jit.cc 中实现了 jit的代码，例如: bool Jit::LoadCompilerLibrary(std::string* error_msg);
+// JIT compiler
+// static void* jit_library_handle_;
+// static void* jit_compiler_handle_;
+// static void* (*jit_load_)(void);
+// static void (*jit_unload_)(void*);
+// static bool (*jit_compile_method_)(void*, ArtMethod*, Thread*, bool, bool);
+// static void (*jit_types_loaded_)(void*, mirror::Class**, size_t count);
+// static void (*jit_update_options_)(void*);
+// static bool (*jit_generate_debug_info_)(void*);
+// template <typename T> static bool LoadSymbol(T*, const char* symbol, std::string* error_msg);
+// nm -g libart.so|grep 'jit_load'
+// 00000000005cf978 B _ZN3art3jit3Jit9jit_load_E
+// nm -g libart.so|grep 'jit_compile'
+// 00000000005cf988 B _ZN3art3jit3Jit19jit_compile_method_E
+// 00000000005cf970 B _ZN3art3jit3Jit20jit_compiler_handle_E
+
+// nm -g libart64.so|grep 'jit_'
+// 00000000006ab610 B _ZN3art3jit3Jit13jit_compiler_E
+// 00000000006ab608 B _ZN3art3jit3Jit19jit_library_handle_E
+// 00000000006ab618 B _ZN3art3jit3Jit9jit_load_E
+// 00000000006a9e90 B _ZN3art5Locks9jit_lock_E
+// 00000000006ac430 B _ZN3art6Thread21jit_sensitive_thread_E
+// 00000000006a7548 D __jit_debug_descriptor
+// 0000000000334f7c T __jit_debug_register_code
+// 00000000006a7540 D __jit_debug_register_code_ptr
+// 00000000006a7538 D g_art_sizeof_jit_code_entry
+// 00000000006a753c D g_art_sizeof_jit_descriptor
+
+// 去掉Dex的Verify功能，避免检查class、method，导致的宿主中的类和patch中类_CF，在寄存器(eg: v2)中不一致，导致检验失败：
+// 发生 java.lang.VerifyError，目前已经用了生成patch(apkpatchplus工程)时，去掉_CF后缀的方式解决，但这不是一个优雅的
+// 方案，更优雅或hack的方法是这里：针对补丁的_CF类及其对应的原始类，直接禁止掉检查。
+// art/runtime/runtime.cc
+// void Runtime::DisableVerifier();
+// 对应art中的符号是：_ZN3art7Runtime15DisableVerifierEv 适配性：支持：android-9/10/11，5/6/7/8方案是: verify_
+// 默认是false 或 verifier::VerifyMode::kNone，所以没问题。
+using DisableVerifier = void (*)(void* runtime); // 参数是Runtime*
+static const char* DisableVerifier_SYM = "_ZN3art7Runtime15DisableVerifierEv";
+// bool Runtime::IsVerificationEnabled() const; 支持 >= andoid-5.0
+using IsVerificationEnabled = bool (*)(void* runtime); // 参数是Runtime*
+static const char* IsVerificationEnabled_SYM = "_ZNK3art7Runtime21IsVerificationEnabledEv";
 
 class Runtime final {
 public:
@@ -66,10 +125,10 @@ public:
 
   ~Runtime() {
     instance_ = nullptr;
-    self_ = nullptr;
+    self_     = nullptr;
   }
 
-  void init(JNIEnv*, const std::shared_ptr<Elf>& elf_op);
+  void init(JNIEnv*, const std::shared_ptr<Elf>& elf_op, int sdkVersionCode);
 
   /**
    * 表示当前运行时Runtime对象地址，相当于
@@ -77,7 +136,7 @@ public:
   void* getRuntime() const noexcept;
   static void* currentThread() noexcept;
 
-  bool useJit() const noexcept;
+  /*bool useJit() const noexcept;*/
 
 private:
   // 代表art虚拟机中当前Runtime对象指针 /art/runtime/runtime.h 中的 Current() 函数:
@@ -85,9 +144,9 @@ private:
   void* instance_;
   static void* self_; // 当前 Thread*
 
-  UseJitCompilation useJitCompilation;
+  /*UseJitCompilation useJitCompilation;*/
 };
 
-} // namespace iwatch
+} // namespace iWatch
 
-#endif //IWATCH_RUNTIME_H
+#endif // IWATCH_RUNTIME_H
